c7d0d3b9c3a6dd6852d8c2acbdbb4fb7ff42aff5
karanhajari <69046337+karanhajari@users.noreply.github.com>
2021-10-17 02:15:40 +0530

    Update StringCalculatorShould.java

--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -3,18 +3,79 @@ package calculator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+public class StringCalculatorTest {
 
-class StringCalculatorShould {
+	@Test
+	public void sumsEmptyStringTo0() {
+		assertThat(StringCalculator.sum(""), is(0));
+	}
 
-    @Test
-    void empty_string_should_return_0() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(0, stringCalculator.add(""));
-    }
+	@Test
+	public void sumsSingleNumberToItself() {
+		assertThat(StringCalculator.sum("5"), is(5));
+		assertThat(StringCalculator.sum("42"), is(42));
+	}
 
-    @Test
-    void string_with_single_number_should_return_number_as_int() {
-        StringCalculator stringCalculator = new StringCalculator();
-        assertEquals(1, stringCalculator.add("1"));
-    }
+	@Test
+	public void sumsTwoNumbersSeperatedByComma() {
+		assertThat(StringCalculator.sum("1,2"), is(3));
+		assertThat(StringCalculator.sum("1,3"), is(4));
+	}
+
+	@Test
+	public void sumsThreeNumbersSeperatedByComma() {
+		assertThat(StringCalculator.sum("1,2,3"), is(6));
+	}
+
+	@Test
+	public void sumsNumbersDelimitedByNewline() {
+		assertThat(StringCalculator.sum("1\n2"), is(3));
+	}
+
+	@Test
+	public void sumsNumbersDelimitedByCommaOrNewline() {
+		assertThat(StringCalculator.sum("1,2\n3"), is(6));
+	}
+
+	@Test
+	public void usesDelimiterSepcified() {
+		assertThat(StringCalculator.sum("//;\n1;2"), is(3));
+		assertThat(StringCalculator.sum("//.\n2.3.1"), is(6));
+	}
+
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+
+	@Test
+	public void throwsOnNegativeNumber() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("negative number: -3");
+
+		StringCalculator.sum("-3");
+	}
+
+	@Test
+	public void throwsOnNegativeNumbersWithAllNumbersInExceptionMessage() {
+		expectedException.expect(IllegalArgumentException.class);
+		expectedException.expectMessage("negative number: -3,-5,-13");
+
+		StringCalculator.sum("1,-3,5,-5,-13");
+	}
+
+	@Test
+	public void mapsNumbersAbove1000ToLastThreeDigits() {
+		assertThat(StringCalculator.sum("1002"), is(2));
+		assertThat(StringCalculator.sum("1040,10002"), is(42));
+	}
+
+	@Test
+	public void acceptsDelimiterOfArbitraryLength() {
+		assertThat(StringCalculator.sum("//[***]\n1***2***3"), is(6));
+	}
+
+	@Test
+	public void acceptsMultipleDelimiters() {
+		assertThat(StringCalculator.sum("//[-][;]\n1-2;3"), is(6));
+		assertThat(StringCalculator.sum("//[--][...]\n2--3...4"), is(9));
+	}
 }
